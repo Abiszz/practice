749e3b2f5e2f5a437dd5435ca6c1bed7b72a8e5d
Abiszz <152966422+Abiszz@users.noreply.github.com>
2023-12-08 13:53:34 +0530

    Update find leap years.py

--- find leap years.py
+++ find leap years.py
@@ -1,20 +1,26 @@
 while True:
     try:
         x = input("Enter The Year")
-        y = len(x)
-        z = x[-1]
-        a = x[-2]
+        if len(x) ==4:
+            y = len(x)
+            z = x[-1]
+            a = x[-2]
 
-        if int(z) == 0 and int(a) == 0:
-            if int(x[:2]) % 4==0:
-                print("leap year")
-            else:
-                print("not leap year")
-        elif int(z) != 0 or int(a) != 0:
-            if int(x[2:]) % 4==0:
-                print("leap year")
-            else:
-                print("not leap year")
+            if int(z) == 0 and int(a) == 0:
+                if int(x[:2]) % 4==0:
+                    print("leap year")
+                else:
+                    print("not leap year")
+            elif int(z) != 0 or int(a) != 0:
+                if int(x[2:]) % 4==0:
+                    print("leap year")
+                else:
+                    print("not leap year")
+        else:
+            print("Enter year only")
+
+
+
     except Exception as e:
         print("Enter year only")
 
