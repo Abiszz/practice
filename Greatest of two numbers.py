1a5c060f60ce5c19b9c8bd57483446ac0ec1cc91
Abiszz <152966422+Abiszz@users.noreply.github.com>
2023-12-08 13:52:32 +0530

    Update Greatest of two numbers.py

--- Greatest of two numbers.py
+++ Greatest of two numbers.py
@@ -1,14 +1,26 @@
-
 while True:
     try:
-        x = int(input("Enter The Number one :"))
-        y = int(input("Enter The Number two:"))
-        num=0
-        if x>y:
-            print(f"{x} is greatest Number")
+        x = input("Enter The Year")
+        if len(x) ==4:
+            y = len(x)
+            z = x[-1]
+            a = x[-2]
+
+            if int(z) == 0 and int(a) == 0:
+                if int(x[:2]) % 4==0:
+                    print("leap year")
+                else:
+                    print("not leap year")
+            elif int(z) != 0 or int(a) != 0:
+                if int(x[2:]) % 4==0:
+                    print("leap year")
+                else:
+                    print("not leap year")
         else:
-            print(f"{y} is greatest Number")
+            print("Enter year only")
+
 
 
     except Exception as e:
-        print("Enter the numbers only")
+        print("Enter year only")
+
